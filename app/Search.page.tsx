6e788d507e652506cfcef792a400ef13dc4925aa
Tarik Raposo Parreira <tarik.parreira@mpdft.mp.br>
2024-12-17 18:27:03 -0300

    [#1] Ajusta imagem de fundo

--- app/Search.page.tsx
+++ app/Search.page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button"
 
 export function Search(){
     return (
-        <div className="search-container m-auto w-full flex items-center justify-center flex-col h-full">
+        <div className="search-container m-auto w-full flex items-center justify-center flex-col h-full gap-8">
            <Image src={logo} alt="QR Code Generator" width={200} height={200}/>
             <div className="input-control flex p-4 gap-6px">
                 <Input type="button" />
