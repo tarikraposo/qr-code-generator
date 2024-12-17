d0c1ff38a766887d43bce7e0001e0aa8d9a79cbb
Tarik Raposo Parreira <tarik.parreira@mpdft.mp.br>
2024-12-17 18:08:25 -0300

    [#1] Create search page

---
+++ app/Search.page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+import { Input } from "@/components/ui/input"
+import logo from '../assets/logo-qr-generator.svg'
+import { Button } from "@/components/ui/button"
+
+export function Search(){
+    return (
+        <div className="search-container m-auto w-full flex items-center justify-center flex-col h-full">
+           <Image src={logo} alt="QR Code Generator" width={200} height={200}/>
+            <div className="input-control flex p-4 gap-6px">
+                <Input type="button" />
+                <Button className="bg-[#3662E3]">QR code</Button>
+            </div>
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
+			bequi: '#3490DC',
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
