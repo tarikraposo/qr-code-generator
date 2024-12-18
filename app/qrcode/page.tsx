eefe946a08013b0419b03e669d6cebfec1a8cc9a
Tarik Raposo Parreira <tarik.parreira@mpdft.mp.br>
2024-12-18 14:34:11 -0300

    [#2] Envolvendo componente de QRCode em um boundary de suspense.

--- app/qrcode/page.tsx
+++ app/qrcode/page.tsx
@@ -1,4 +1,6 @@
+// filepath: /c:/Users/tarik.parreira/Documents/estudos/react/qr-code/app/qrcode/page.tsx
 "use client"
+import { Suspense } from "react";
 import { Button } from "@/components/ui/button";
 import { useRouter, useSearchParams } from "next/navigation"
 import { QRCodeSVG } from "qrcode.react"
@@ -6,7 +8,7 @@ import { ArrowDownToLine, Share2 } from "lucide-react";
 import logo from '../../assets/logo-qr-generator.svg'
 import Image from "next/image";
 
-export default function Qrcode() {
+function QrcodeContent() {
     const router = useRouter();
     const params = useSearchParams();
     const link = params.get('link');
@@ -15,9 +17,9 @@ export default function Qrcode() {
         <div className="container m-auto w-full flex items-center justify-center flex-col h-full gap-8 relative">
             <div className="qr-code-control flex p-4 space-y-16 flex-col items-center">
             <Image src={logo} alt="QR Code Generator" width={100} height={100} className="absolute top-10 left-10"/>
-            {link? (
+            {link ? (
                 <QRCodeSVG value={link} size={256} className="qr-code-svg"/>
-            ):(
+            ) : (
                 <p>No link provided</p>
             )}
             <div className="flex gap-16">
@@ -29,3 +31,10 @@ export default function Qrcode() {
     )
 }
 
+export default function Qrcode() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <QrcodeContent />
+        </Suspense>
+    );
+}
