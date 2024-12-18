558f84314deb257ef4f23c1d21e1c3739584d818
Tarik Raposo Parreira <tarik.parreira@mpdft.mp.br>
2024-12-18 14:00:59 -0300

    [#2] Instala biblioteca qr-code, finaliza configuração do QRCode.

--- app/qrcode/page.tsx
+++ app/qrcode/page.tsx
@@ -1,13 +1,31 @@
 "use client"
-import { useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button";
+import { useRouter, useSearchParams } from "next/navigation"
+import { QRCodeSVG } from "qrcode.react"
+import { ArrowDownToLine, Share2 } from "lucide-react";
+import logo from '../../assets/logo-qr-generator.svg'
+import Image from "next/image";
 
 export default function Qrcode() {
     const router = useRouter();
+    const params = useSearchParams();
+    const link = params.get('link');
 
     return (
-        <div className="search-container m-auto w-full flex items-center justify-center flex-col h-full gap-8">
-            <h1>QR Code Page</h1>
-            <button onClick={() => router.push('/')}>Go Back</button>
+        <div className="container m-auto w-full flex items-center justify-center flex-col h-full gap-8 relative">
+            <div className="qr-code-control flex p-4 space-y-16 flex-col items-center">
+            <Image src={logo} alt="QR Code Generator" width={100} height={100} className="absolute top-10 left-10"/>
+            {link? (
+                <QRCodeSVG value={link} size={256} className="qr-code-svg"/>
+            ):(
+                <p>No link provided</p>
+            )}
+            <div className="flex gap-16">
+                <Button className="bg-[#3662E3] button" onClick={() => router.back()}>Download<ArrowDownToLine /></Button>
+                <Button className="bg-[#3662E3] button" onClick={() => router.back()}>Share<Share2 /></Button>
+            </div>
+            </div>
         </div>
     )
 }
+
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from "next/image"
+import { useEffect, useRef, useState } from "react"
 import { useRouter } from "next/navigation"
 
 import { Input } from "@/components/ui/input"
@@ -7,14 +8,31 @@ import logo from '../../assets/logo-qr-generator.svg'
 import { Button } from "@/components/ui/button"
 
 export function Search(){
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, []);
+    const [link, setLink] = useState('');
     const router = useRouter();
 
+    const handleGenerateQr = () => {
+        router.push(`/qrcode?link=${encodeURIComponent(link)}`);
+    }
+
     return (
-        <div className="search-container m-auto w-full flex items-center justify-center flex-col h-full gap-8">
+        <div className="container m-auto w-full flex items-center justify-center flex-col h-full gap-8">
            <Image src={logo} alt="QR Code Generator" width={200} height={200}/>
             <div className="input-control flex p-4 gap-6px">
-                <Input type="button" />
-                <Button className="bg-[#3662E3]" onClick={() => router.push('/qrcode')}>QR code</Button>
+                <Input className="h-100 color-white" 
+                ref={inputRef}
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+                placeholder="Digite o link aqui"
+                />
+                <Button className="bg-[#3662E3]" onClick={handleGenerateQr}>QR code</Button>
             </div>
         </div>
     )
